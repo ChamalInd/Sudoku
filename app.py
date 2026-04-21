ef7416d93e3b14342028c0b6e5c24a9fe7ebea8e
Chamal Induwara <88148370+ChamalInd@users.noreply.github.com>
2026-04-21 22:53:59 +0530

    Sudoku logic is almostt complete

--- app.py
+++ app.py
@@ -1,6 +1,11 @@
-from flask import Flask, render_template, request
+from flask import Flask, render_template, request, jsonify
 
-from helper.board import generate
+from helper.board import generate, print_board
+
+# defining global variables 
+game_board = None
+full_board = None
+difficulty = None
 
 
 # configuring app
@@ -25,17 +30,25 @@ def index():
 
 @app.route('/game', methods=['POST', 'GET'])
 def game():
+    global game_board, full_board, difficulty
+
     if request.method == 'GET':
         # checking for difficulty level 
         levels = ['easy', 'medium', 'hard', 'extreme']
         difficulty = request.args.get('difficulty')
         difficulty_level = levels.index(difficulty)
-    
-    if request.method == 'POST':
-        btn = request.form.get('clicked-btn')
-        print(btn)
-
-    # generating the board 
-    full_board, game_board = generate(difficulty_level)
+        
+        # generating the board 
+        full_board, game_board = generate(difficulty_level)
+        print_board(full_board)
 
     return render_template('game.html', board=game_board, difficulty=difficulty)
+
+@app.route('/update-board', methods=['POST'])
+def update_game_board():
+    board = request.get_json()['current_state']
+    if board == full_board:
+        print('well done')
+        
+    
+    return jsonify({'status': 'updated'})
--- static/script.js
+++ static/script.js
@@ -1,5 +1,18 @@
 let time = 0;
 let interval;
+let selectedBtn = null
+const btns = document.querySelectorAll('.sudoku-btns');
+let gameBoard = [
+    [['', '', ''], ['', '', ''], ['', '', '']],
+    [['', '', ''], ['', '', ''], ['', '', '']],
+    [['', '', ''], ['', '', ''], ['', '', '']],
+    [['', '', ''], ['', '', ''], ['', '', '']],
+    [['', '', ''], ['', '', ''], ['', '', '']],
+    [['', '', ''], ['', '', ''], ['', '', '']],
+    [['', '', ''], ['', '', ''], ['', '', '']],
+    [['', '', ''], ['', '', ''], ['', '', '']],
+    [['', '', ''], ['', '', ''], ['', '', '']]
+]
 
 // increment timer 
 function gamePageTimmer() {    
@@ -25,3 +38,88 @@ document.addEventListener('DOMContentLoaded', () => {
         gamePageTimmer();
     }
 });
+
+// get selected box in sudoku board 
+function buttonSelect(button) {
+    // check for a currently active button 
+    const currentSelected = document.querySelector('.sudoku-btns.active');
+    
+    if (currentSelected) {
+        currentSelected.classList.remove('active');
+        for (let i = 0; i < btns.length; i++) {
+            btns[i].classList.remove('others');
+        }
+    }
+
+    // selecting the button 
+    selectedBtn = button;
+    selectedBtn.classList.add('active');
+
+    // highlighting the impact buttons
+    let current = button.id.split('x').map(Number);
+    
+    // filling rows 
+    for (let i = 0; i < 3; i++) {
+        for (let j = 0; j < 3; j++) {
+            id = `${current[0]}x${i}x${j}`;
+            document.getElementById(id).classList.add('others');
+        }
+    }
+
+    // filling columns 
+    for (let i = 0; i < 9; i++) {
+        id = `${i}x${current[1]}x${current[2]}`;
+        document.getElementById(id).classList.add('others');
+    }
+
+    // filling sections 
+    let row = Math.trunc(current[0] / 3) * 3;
+    for (let i = row; i < row + 3; i++) {
+        for (let j = 0; j < 3; j++) {
+            id = `${i}x${current[1]}x${j}`;
+            document.getElementById(id).classList.add('others');
+        }
+    }
+
+}
+
+// enter or clear values from the board 
+function keyPadAction(button) {
+    let text = button.id;
+    const numbers = ['1', '2', '3', '4', '5', '6', '7', '8', '9']
+
+    if (numbers.includes(text)){
+        selectedBtn.innerText = text;
+    } else if (text === 'c') {
+        selectedBtn.innerText = '';
+    }
+
+    syncWithServer();
+    
+}
+
+// syncing the webpage with flask app 
+async function syncWithServer() {
+    // updating game board 
+    for (let i = 0; i < btns.length; i ++) {
+        let index = btns[i].id.split('x').map(Number);
+        if (btns[i].innerText === '') {
+            gameBoard[index[0]][index[1]][index[2]] = btns[i].innerText;
+        } else {
+            gameBoard[index[0]][index[1]][index[2]] = Number(btns[i].innerText);
+        }
+        // alert(gameBoard[index[0]][index[1]][index[2]]);
+    }
+
+    // alert(gameBoard[8][2][2]);
+
+    try {
+        await fetch('/update-board', {
+            method: 'POST',
+            headers: {'Content-Type': 'application/json'},
+            body: JSON.stringify({ current_state: gameBoard })
+        });
+    } catch (error) {
+        alert('Sync faild ' + error);
+    }
+}
